67b2cf7038856b97814d7876504f79c90886c237
Line Kargaard <lika62310@edu.ucl.dk>
2024-04-09 11:07:17 +0200

    Version 1

---
+++ Uge 15.1/Disaheim/Amulet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Amulet
+    {
+        public string ItemId { get; set; }
+        public string Design {  get; set; }
+        public Level Quality { get; set; }
+
+        public Amulet(string itemId, Level quality, string design)
+        {
+            ItemId = itemId;
+            Design = design;
+            Quality = quality;
+        }
+
+        public Amulet(string itemId)
+        {
+            ItemId = itemId;
+            Quality = Level.medium;
+        }
+
+        public Amulet (string itemId, Level quality)
+        {
+            ItemId = itemId;
+            Quality = quality;
+        }
+
+        public string ToString()
+        {
+            string s = $"ItemId: {ItemId}, Quality: {Quality}, Design: {Design}";
+            return s;
+        }
+    }
+}
---
+++ Uge 15.1/Disaheim/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Book
+    {
+        public string ItemId { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+
+        public Book(string itemId, string title, double price)
+        {
+            ItemId = itemId;
+            Title = title;
+            Price = price;
+        }
+        public Book (string itemId)
+        {
+            ItemId = itemId;
+        }
+
+        public Book (string itemId, string title)
+        {
+            ItemId = itemId;
+            Title = title;
+        }
+
+        //("ItemId: 11, Quality: medium, Design: ")
+
+        public string ToString()
+        {
+            string s =$"ItemId: {ItemId}, Title: {Title}, Price: {Price}";
+            return s;
+        }
+    }
+}
